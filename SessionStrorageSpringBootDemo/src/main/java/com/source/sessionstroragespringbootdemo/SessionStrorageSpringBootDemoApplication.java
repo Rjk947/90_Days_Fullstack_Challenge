195fcf60d786afda6bdf4a55cee2378ee8096649
Rajesh Kumar <rajesh8507kumar@gmail.com>
2025-06-30 00:01:30 +0530

    added

---
+++ SessionStrorageSpringBootDemo/src/main/java/com/source/sessionstroragespringbootdemo/SessionStrorageSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.source.sessionstroragespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionStrorageSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionStrorageSpringBootDemoApplication.class, args);
+    }
+
+}
